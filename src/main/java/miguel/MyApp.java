e34b949d27106ffdfa3e62eacf406cc236ba7fa9
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2018-08-09 15:50:00 +0200

    Initial commit

---
+++ src/main/java/miguel/MyApp.java
@@ -0,0 +1,25 @@
+package miguel;
+
+import miguel.model.Person;
+import io.mateu.mdd.core.annotations.Action;
+import io.mateu.mdd.core.annotations.Caption;
+import io.mateu.mdd.core.app.AbstractAction;
+import io.mateu.mdd.core.app.MDDOpenCRUDAction;
+import io.mateu.mdd.core.app.SimpleMDDApplication;
+
+public class MyApp extends SimpleMDDApplication {
+
+    @Action(value = "Say hello", order = 1)
+    public String hello(String name) {
+        return "Hello " + name + "!";
+    }
+
+
+    @Action(order = 2)
+    public AbstractAction persons() {
+        return new MDDOpenCRUDAction(Person.class);
+    }
+
+
+
+}
