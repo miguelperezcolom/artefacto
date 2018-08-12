16a9f98d674e98b944878eb8807308606f5e908e
miguelperezcolom <miguelperezcolom@gmail.com>
2018-08-12 20:53:51 +0200

    aa

--- src/main/java/miguel/JCEEncryptor.java
+++ src/main/java/miguel/JCEEncryptor.java
@@ -5,11 +5,13 @@ import org.eclipse.persistence.internal.security.Securable;
 public class JCEEncryptor implements Securable {
     @Override
     public String encryptPassword(String s) {
+        System.out.println("encrypt " + s + " to " + s);
         return s;
     }
 
     @Override
     public String decryptPassword(String s) {
+        System.out.println("decrypt " + s + " to " + s);
         return s;
     }
 }
